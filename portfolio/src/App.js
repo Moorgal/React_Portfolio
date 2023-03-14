66a7a2444d57d5b951a61bfd30bdf91a59e84a7d
Moorgal <megaboki@gmail.com>
2023-03-14 00:06:05 +0000

    about me

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Aboutme from './components/Aboutme';
 
 function App() {
   console.log('hello');
@@ -8,6 +9,7 @@ function App() {
     <>
       <Navbar />
       <Hero />
+      <Aboutme />
     </>
   );
 }
---
+++ portfolio/src/components/Aboutme.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Aboutme = () => {
+  return (
+    <section className="section" id="about">
+      <section className="section-center">
+        <div className="section-title">
+          <h2>About Me</h2>
+        </div>
+        <div className="section-article">
+          <p className="paragraph">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quia rem non voluptatum culpa tenetur autem maxime incidunt nemo debitis eligendi, iure ipsa rerum dolores repellat
+            accusamus temporibus. Reprehenderit, quam. Cumque maiores placeat dolore quibusdam! Magnam recusandae id dignissimos praesentium? Optio commodi ipsam a aut assumenda corrupti maiores
+            accusamus praesentium repellat esse illo minima eos, harum reprehenderit officia. Atque, suscipit.
+          </p>
+          <br />
+          <p className="paragraph">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quia rem non voluptatum culpa tenetur autem maxime incidunt nemo debitis eligendi, iure ipsa rerum dolores repellat
+            accusamus temporibus. Reprehenderit, quam. Cumque maiores placeat dolore quibusdam! Magnam recusandae id dignissimos praesentium?
+          </p>
+        </div>
+      </section>
+    </section>
+  );
+};
+
+export default Aboutme;
